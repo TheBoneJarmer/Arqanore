b2f8f66500fe361bbcff549ca6ee788a13fdd9f8
TheBoneJarmer <rmlabruyere@gmail.com>
2019-10-08 07:09:19 +0200

    Added platformer example

---
+++ examples/platformer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Seanuts.Framework;
+
+namespace example3
+{
+    class Program
+    {
+        static GameWindow Window { get; set; }
+        
+        static void Main(string[] args)
+        {
+            Window = new GameWindow(800, 600, "Platformer");
+            Window.Open();
+        }
+    }
+}
