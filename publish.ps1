3c86528007911f8af4536ac74743b6c401058709
Ruben Labruyere <info@rubenlabruyere.be>
2021-02-23 23:36:36 +0100

    Added publish script in powershell

---
+++ publish.ps1
@@ -0,0 +1,17 @@
+﻿$branch = git branch --show-current;
+
+Remove-Item *.nupkg
+dotnet pack -c Release -o . .\src\Arqanore\Arqanore.csproj
+
+if ($branch -eq "develop")
+{
+    dotnet nuget push -s github *.nupkg
+}
+elseif ($branch -eq "main")
+{
+    dotnet nuget push -s nuget.org -k $env:NUGET_API_KEY *.nupkg
+}
+else
+{
+    echo "Can only publish nuget package on develop or main branch";
+}
