5d5dac9526a46d44b27963e9844568c563db2906
Ruben Labruyere <info@rubenlabruyere.be>
2021-02-25 23:48:38 +0100

    Updated scripts

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,7 @@
 ﻿$branch = git branch --show-current;
 
 Remove-Item *.nupkg
-dotnet pack -c Release -o . .\src\Arqanore\Arqanore.csproj
+dotnet pack -c Release -o . ./src/*.csproj
 
 if ($branch -eq "develop")
 {
